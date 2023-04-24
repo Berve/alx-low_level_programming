76e3c21ead81a58374d95a45862ab34f8fd199c3
Berve <bervely.vanderpuye@gmail.com>
2023-04-24 09:15:32 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
