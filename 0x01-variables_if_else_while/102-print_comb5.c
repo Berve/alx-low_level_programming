db84f2350c64876a9e51c6c68262bd83f83a3017
Berve <bervely.vanderpuye@gmail.com>
2023-04-24 15:57:59 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,19 +8,20 @@
 int main(void)
 {
 int p, q;
-for (p = 0; p <= 98; p++)
+for (p = 0; p <= 99; p++)
 {
-for (q = p + 1; q <= 98; q++)
+for (q = 0; q <= 99; q++)
 {
 putchar((p / 10) + '0');
 putchar((p % 10) + '0');
 putchar(' ');
 putchar((q / 10) + '0');
 putchar((q % 10) + '0');
-if (p == 98 && q == 99)
-continue;
+if (p != 98 && q != 99)
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
