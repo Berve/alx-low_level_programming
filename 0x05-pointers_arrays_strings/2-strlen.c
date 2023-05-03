bc13d4b03c2c008a0493e82342a5f050c1ac77ba
Berve <bervely.vanderpuye@gmail.com>
2023-05-03 00:49:27 +0000

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * _strlen - a function that returns the length of a string.
+ * @a: character
+ * Return: void
+ */
+
+int _strlen(char *p)
+{
+	int n = 0;
+
+	for (; *p++;)
+	n++;
+	return (n);
+}
