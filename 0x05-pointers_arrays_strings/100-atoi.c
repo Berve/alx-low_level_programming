7f93e8219e8d9b7563e9552e5e099c0a65d4b3e9
Berve <bervely.vanderpuye@gmail.com>
2023-05-02 19:25:47 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,18 +12,18 @@ int _atoi(char *s)
 	int min = 1;
 	int isi = 0;
 
-	while (s[c])
+	while (s[i])
 	{
-		if (s[c] == 45)
+		if (s[i] == 45)
 		{
 			min *= -1;
 		}
 
-		while (s[c] >= 48 && s[c] <= 57)
+		while (s[i] >= 48 && s[i] <= 57)
 		{
 			isi = 1;
-			nb= (nb* 10) + (s[c] - '0');
-			c++;
+			nb= (nb* 10) + (s[i] - '0');
+			i++;
 		}
 
 		if (isi == 1)
@@ -31,7 +31,7 @@ int _atoi(char *s)
 			break;
 		}
 
-		c++;
+		i++;
 	}
 
 	nb *= min;
