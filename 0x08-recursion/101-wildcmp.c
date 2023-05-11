f939bf847e486eefe6af50f2556704027f386273
Berve <bervely.vanderpuye@gmail.com>
2023-05-11 02:45:49 +0000

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * str_checker - check if two strings are identical.
- * @s1: string_1 base address.
- * @s2: string_2 base address.
- * @i: left index.
- * @j: special index. (joker)
+ * checker - A function to check if two strings are identical.
+ * @s1: Parameter 1.
+ * @s2: Parameter 2
+ * @i: Parameter 3
+ * @j: Parameter 4
  * Return: 1 if s is palindrome, 0 otherwise.
  */
-int str_checker(char *s1, char *s2, int i, int j)
+int checker(char *s1, char *s2, int i, int j)
 {
 	if (s1[i] == '\0' && s2[j] == '\0')
 		return (1);
 	if (s1[i] == s2[j])
-		return (str_checker(s1, s2, i + 1, j + 1));
+		return (checker(s1, s2, i + 1, j + 1));
 	if (s1[i] == '\0' && s2[j] == '*')
-		return (str_checker(s1, s2, i, j + 1));
+		return (checker(s1, s2, i, j + 1));
 	if (s2[j] == '*')
-		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+		return (checker(s1, s2, i + 1, j) || checker(s1, s2, i, j + 1));
 	return (0);
 }
 /**
@@ -29,5 +29,5 @@ int str_checker(char *s1, char *s2, int i, int j)
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (str_checker(s1, s2, 0, 0));
+	return (checker(s1, s2, 0, 0));
 }
