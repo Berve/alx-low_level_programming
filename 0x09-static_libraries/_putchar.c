f51305f63e250acbe3e12d2ea3556c6579d4ca7e
Berve <bervely.vanderpuye@gmail.com>
2023-05-12 23:30:03 +0000

    Static variables

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: First parameter
+ * Return: 1 IF SUCCESSFUL
+ * On error, RETURN -1, and errno is set appropriately
+ */
+
+int _putchar(char c)
+
+{
+
+		return (write(1, &c, 1));
+
+}
