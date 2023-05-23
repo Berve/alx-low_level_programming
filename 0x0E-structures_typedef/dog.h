cac6416993bfb621826398013f60c23a34a20642
Berve <bervely.vanderpuye@gmail.com>
2023-05-23 23:48:47 +0000

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Structure with variable dog
+ * @name: a pointer to a character
+ * @age: a floating point
+ * @owner: a pointer to a chararcter
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
