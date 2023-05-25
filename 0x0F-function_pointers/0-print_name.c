2f5ade99e2c8a4a3df620965cd6c5550a2722783
Berve <bervely.vanderpuye@gmail.com>
2023-05-25 01:03:54 +0000

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function to print name
+ * @name: 1st parameter
+ * @f: name of function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		
+		*f(name);
+}
